5abc1478ebc15b3dc14de51aad9f13c3a9adb931
aman12-10-12 <aman4483raj@gmail.com>
2026-09-16 19:16:54 +0530

    feat : Add dashboard sidebar navigation and layout updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Layout from "./components/Layout";
+import Accounts from "./pages/Accounts";
+import Dashboard from "./pages/Dashboard";
+import Scheduler from "./pages/Scheduler";
+import AIComposer from "./pages/AIComposer";
 
 export default function App() {
     return (
@@ -10,6 +15,12 @@ export default function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route element={<Layout></Layout>} >
+                    <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+                    <Route path="/accounts" element={<Accounts></Accounts>} />
+                    <Route path="/schedule" element={<Scheduler></Scheduler>} />
+                    <Route path="ai-composer" element={<AIComposer></AIComposer>} />
+                </Route>
             </Routes>
         </>
     );
