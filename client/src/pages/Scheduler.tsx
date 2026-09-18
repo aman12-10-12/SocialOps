a8a66cb6ed1b4b268b8902a51d912870e55c240e
aman12-10-12 <aman4483raj@gmail.com>
2026-09-18 20:38:06 +0530

    feat : add scheduler page UI

--- client/src/pages/Scheduler.tsx
+++ client/src/pages/Scheduler.tsx
@@ -1,8 +1,167 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { dummyPostsData, PLATFORMS } from "../assets/assets";
+import { ArrowRightIcon, CalendarIcon, ClockIcon, XIcon } from "lucide-react";
+
 
 const Scheduler = () => {
+
+  const [posts, setPosts] = useState<any[]>([]);
+  const [content, setContent] = useState("");
+  const [scheduledDate, setScheduledDate] = useState("")
+  const [scheduledTime, setScheduledTime] = useState("")
+  const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([])
+  const [mediaFile, setMediaFile] = useState<File | null>(null)
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    setPosts(dummyPostsData)
+  }
+
+  useEffect(() => {
+    (async () => await fetchPosts())();
+    const interval = setInterval(async ()=> await fetchPosts(), 1000);
+    return () => clearInterval(interval);
+
+  },[])
+
+  const scheduled = posts.filter((p) => p.status === "scheduled")
+  const published = posts.filter((p) => p.status === "published")
+
+  const togglePlatform = (id: string) => setSelectedPlatforms((prev) => (prev.includes(id) ? prev.filter((p) => p !== id) : [...prev, id]))
+
+  const handleSchedule = async (e : React.FormEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setPosts((prev)=> [...prev, dummyPostsData[0]])
+    }, 1000) 
+  }
+
+
   return (
-    <div>Scheduler</div>
+    <div className="flex flex-col lg:flex-row gap-6 h-full">
+      
+      {/* Compose panel */}
+
+      <div className="w-full lg:w-[460px] shrink-0">
+        <div className="bg-white rounded-2xl border border-slate-200 p-6">
+          <div className="flex items-center gap-2 mb-6">
+            <h2 className="text-lg text-slate-700">Compose Post</h2>
+          </div>
+
+          <form className="space-y-5" onSubmit={handleSchedule}>
+            {/* Platform */}
+
+            <div>
+              <label className="block text-xs text-slate-500 uppercase mb-2">Platforms</label>
+              <div className="flex flex-wrap gap-3">
+                {PLATFORMS.map((p) => {
+                  const active = selectedPlatforms.includes(p.id)
+                  return (
+                    <button key={p.id} type="button" 
+                    onClick={() => togglePlatform(p.id)}
+                    className={`flex items-center gap-1.5 p-3 rounded-md border transition-all dduration-150 ${active ? "bg-red-50 border-red-300 text-red-500 scale-103" : "border-slate-200 text-slate-500 hover:border-slate-300"}`}>
+                      <p.icon className="size-4.5" />
+                    </button>
+                  )
+                })}
+              </div>
+            </div>
+
+            {/* Content */}
+
+            <div>
+              <label className="block text-xs text-slate-500 uppercase mb-2">Content</label>
+              <textarea  required rows={5} placeholder="What do you want to share today ?"
+              className="w-full px-5 py-4 bg-slate-50 border border-slate-200 rounded-2xl text-slate-900 text-sm placeholder-slate-400 outline-none resize-none"
+              value={content} onChange={(e)=> setContent(e.target.value)}/>
+
+              <div className={`text-right text-xs mt-1 font-medium ${content.length > 270 ? "text-red-500" : "text-slate-400"}`}>
+                {content.length}/280
+              </div>
+            </div>
+
+            {/* Media upload */}
+
+            <div>
+              <label className="block text-xs text-slate-500 uppercase mb-2">Media (Optional)</label>
+              {mediaFile ? (
+                <div className="relative rounded-xl overflow-hidden border border-slate-200 bg-slate-50">
+                  {mediaFile.type.startsWith("image/") 
+                  ? 
+                  <img src={URL.createObjectURL(mediaFile)} alt="preview" className="w-full h-40 object-cover"/> 
+                  : 
+                  <video src={URL.createObjectURL(mediaFile)} className="w-full h-40 object-cover" controls/>}
+
+                  <button type="button" 
+                  onClick={()=>setMediaFile(null)}
+                  className="absolute top-2 right-2 size-7 bg-slate-900/60 hover:bg-slate-900/80 text-white rounded-full flex items-center justify-center transition-colors">
+                    <XIcon className="size-3.5"/>
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <label className="flex items-center justify-center gap-2 p-5 py-10 border-2 border-dashed border-slate-200 rounded-xl cursor-pointer hover:border-red-300 hover:bg-red-50/30 transition-all group">
+                    <span className="text-sm text-slate-500 group-hover:text-red-600 transition-colors">
+                      Click to upload Image or Video
+                    </span>
+                    <input type="file" accept="image/*,Video/*" className="hidden"
+                    onChange={(e)=>e.target.files?.[0] && setMediaFile(e.target.files[0])}/>
+                  </label>
+                </div>
+              )}
+            </div>
+
+            {/* Date & Time */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+              <div>
+                <label className="block text-xs text-slate-500 uppercase mb-2">Date</label>
+                <div className="relative">
+                  <CalendarIcon className="size-4 absolute left-3 top-1/2 -translate-y-1/2 text-slate-400 pointer-events-none"/>
+                  <input type="date" required className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-lg text-slate-900 text-sm outline-none"
+                  value={scheduledDate} onChange={(e)=> setScheduledDate(e.target.value)}/>
+                </div>
+              </div>
+              <div>
+                <label className="block text-xs text-slate-500 uppercase mb-2">Date</label>
+                <div className="relative">
+                  <ClockIcon className="size-4 absolute left-3 top-1/2 -translate-y-1/2 text-slate-400 pointer-events-none"/>
+                  <input type="time" required className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-lg text-slate-900 text-sm outline-none"
+                  value={scheduledTime} onChange={(e)=> setScheduledTime(e.target.value)}/>
+                </div>
+              </div>
+            </div>
+
+            {/* submit */}
+            <button type="submit" disabled={loading} 
+            className="w-full flex items-center justify-center gap-2 py-3.5 bg-red-500 hover:bg-red-600 transition-all text-white rounded-lg">
+              {loading ? (
+                <>
+                <div className="size-4 border-2 border-white border-t-transparent rounded-full animate-spin"/>
+                Scheduling...
+                </>
+              ) : (
+                <>
+                Scheduled Post
+                <ArrowRightIcon className="size-4"/>
+                </>
+              )}
+            </button>            
+          </form>
+        </div>   
+      </div>
+
+      {/* Queue panels */}
+
+      <div className="flex-1 flex flex-col gap-6 min-w-0">
+        {/* Upcoming */}
+
+        {/* Published */}
+      </div>
+
+
+    </div>
   )
 }
 
