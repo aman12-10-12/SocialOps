0da58d7d7ed87e92c4faca1d99bf617b2a666a56
aman12-10-12 <aman4483raj@gmail.com>
2026-09-20 14:58:22 +0530

    feat : add auth routes

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,12 +14,15 @@ await connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req: Request, res: Response) => {
     res.send("Server is Live!");
 });
+
+app.use("/api/auth", authRouter);
 
 // Global Error Handler
 app.use((err: any, _req: Request, res: Response, _next: NextFunction)=> {
