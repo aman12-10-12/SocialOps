30e1b7bee4126cc6c3bf36f4e4f64b5a1a86b336
aman12-10-12 <aman4483raj@gmail.com>
2026-09-22 00:37:56 +0530

    feat(controllers) : add get , add and diconnect account controllers

---
+++ server/controllers/accountControllers.ts
@@ -0,0 +1,93 @@
+import { Response } from "express";
+import { AuthRequest } from "../middlewares/authMiddleware.js";
+import { Account } from "../models/Accounts.js";
+import zernio from "../config/zernio.js";
+
+
+/**
+ * @name getAccounts
+ * @description get all accounts
+ * @access Private
+ * @route GET /api/accounts
+ */
+export const getAccounts = async (req: AuthRequest, res: Response) : Promise<void> => {
+    try {
+        const accounts = await Account.find({user: req.user._id})
+        res.json(accounts)
+    }catch(error: any) {
+        res.status(500).json({
+            message : error?.message || "server error"
+        })
+    }
+}
+
+
+/**
+ * @name addAccount
+ * @description add accounts
+ * @access Private
+ * @route GET /api/accounts
+ */
+export const addAccount = async (req: AuthRequest, res: Response) : Promise<void> => {
+    try {
+        const {platform, handle, avatarUrl} = req.body
+
+        const account = await Account.create({
+            user: req.user._id,
+            platform,
+            handle,
+            avatarUrl
+        });
+        res.status(201).json(account)
+    }catch(error: any) {
+        res.status(500).json({
+            message : error?.message || "server error"
+        })
+    }
+}
+
+
+/**
+ * @name discconectAccount
+ * @description use to discconect the account
+ * @access Private
+ * @route DELETE /api/accounts/:id
+ */
+export const discconectAccount = async (req: AuthRequest, res: Response) : Promise<void> => {
+    try {
+       const account = await Account.findOne({
+            _id: req.params.id,
+            user: req.user._id
+        })
+
+       if(!account) {
+            res.status(404).json({
+                message: "Account not found"
+            })
+            return;
+       }
+       if(account.zernioAccountId) {
+            try {
+                await zernio.accounts.deleteAccount({
+                    path : {
+                        accountId: account.zernioAccountId
+                    }
+                })
+            }catch (error : any) {
+                res.status(500).json({
+                    message : error?.message || "server error"
+                })
+                return;
+            }
+        }
+
+        await account.deleteOne()
+        res.json({
+            message : "Account diconnected Successfully"
+        })
+    }catch(error: any) {
+        res.status(500).json({
+            message : error?.message || "server error"
+        })
+    }
+}
