8938dd326b10981e4bdbb28a6230e7fa6e2810ad
aman12-10-12 <aman4483raj@gmail.com>
2026-09-22 22:14:38 +0530

    feat(controllers/postController) : add schedule post controller

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -164,4 +164,61 @@ export const getPosts = async (req: AuthRequest, res: Response): Promise<void> =
 }
 
 
+/**
+ * @name schedulePost
+ * @description schedule posts
+ * @access Private
+ * @route POST /api/posts/
+ */
+export const schedulePost = async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+        const {content, platforms, scheduledFor, status } = req.body;
 
+        // Parse platforms it it comes as a strigified array from formData
+        let parsedPlatforms = platforms;
+        if(typeof platforms === "string") {
+            try {
+                parsedPlatforms = JSON.parse(platforms)
+            } catch (e) {
+                parsedPlatforms = platforms.split(",")
+            }
+        }
+
+        let mediaUrl: string | undefined = req.body.mediaUrl;
+        let mediaType: "image" | "video" | undefined = req.body.mediaType
+
+        if(req.file) {
+            const result = await new Promise<any>((resolve, reject)=> {
+                const stream = cloudinary.uploader.upload_stream({
+                    resource_type: "auto",
+                    folder: "SocialOps"
+                }, (error, result) => {
+                    if(error) {
+                        reject(error)
+                    }
+                    else {
+                        resolve(result)
+                    }
+                });
+                stream.end(req.file!.buffer)
+            });
+            mediaUrl = result.secure_url;
+            mediaType = result.resource_type === "video" ? "video" : "image"
+        }
+
+        const post = await Post.create({
+            user: req.user._id,
+            content,
+            platforms: parsedPlatforms,
+            mediaUrl,
+            mediaType,
+            scheduledFor,
+            status
+        })
+
+        res.status(201).json(post)
+
+    } catch (error: any) {
+        res.status(500).json({ message: error?.message || "Server Error" });
+    }
+}
